ff63b8716cd281164eeec63e81e160456f38f098
Tiziana Mancini <tizianabmancini@gmail.com>
2022-02-11 14:04:52 -0500

    Added GitHub and Netlify links

--- src/App.js
+++ src/App.js
@@ -10,7 +10,24 @@ export default function App() {
         <Dictionary />
       </div>
       <footer className="App-footer">
-        <small>Coded by Tiziana Mancini</small>
+        <small>
+          Coded by Tiziana Mancini using{" "}
+          <a
+            href="https://github.com/TizianaMancini/react-dictionary-project"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced code
+          </a>{" "}
+          and hosted on{" "}
+          <a
+            href="https://hopeful-agnesi-017cd9.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Netlify.
+          </a>
+        </small>
       </footer>
     </div>
   );
