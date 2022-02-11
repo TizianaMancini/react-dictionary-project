07177cbfaf1f5786fce9aeee24686e96ba82d75e
Tiziana Mancini <tizianabmancini@gmail.com>
2022-02-11 11:58:20 -0500

    Created dictionary component and search bar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <header className="App-header">Dictionary</header>
+
+        <Dictionary />
+      </div>
+      <footer className="App-footer">
+        <small>Coded by Tiziana Mancini</small>
+      </footer>
+    </div>
+  );
 }
