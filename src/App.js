e4d1c43b0af8c80df148e06dbef4655bbec63213
Tiziana Mancini <tizianabmancini@gmail.com>
2022-02-13 15:58:31 -0500

    Added emoji

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <header className="App-header">Dictionary</header>
+        <header className="App-header">Dictionary 📚</header>
 
         <Dictionary defaultKeyword="karate" />
       </div>
